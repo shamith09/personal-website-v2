098a5eb704ef32b3bb8a1c89c3fd5cd373eb02ae
Shamith Pasula <shamith09@berkeley.edu>
2025-01-22 16:56:55 -0800

    pointer

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -3,16 +3,21 @@ import Link from "next/link";
 import GlitchText from "react-glitch-effect/core/GlitchText";
 
 const greetings = [
-  "Hi, I'm",
-  "Hola, soy",
-  "你好，我是",
-  "नमस्ते, मैं हूं",
-  "Bonjour, je suis",
-  "こんにちは、私は",
-  "Olá, eu sou",
-  "Ciao, sono",
-  "Hallo, ich bin",
-  "안녕하세요, 저는",
+  { greeting: <>Hi, I&apos;m</>, punctuation: <>.</> },
+  { greeting: <>Hola, soy</>, punctuation: <>.</> },
+  { greeting: <>#define</>, punctuation: <>;</> },
+  { greeting: <>こんにちは、私は</>, punctuation: <>。</> },
+  {
+    greeting: <span className="font-mono">var me =</span>,
+    punctuation: <span className="font-mono">;</span>,
+  },
+  { greeting: <>नमस्ते, मैं हूं</>, punctuation: <>।</> },
+  { greeting: <>你好，我是</>, punctuation: <>。</> },
+  { greeting: <>Bonjour, je suis</>, punctuation: <>.</> },
+  { greeting: <>Olá, eu sou</>, punctuation: <>.</> },
+  { greeting: <>Ciao, sono</>, punctuation: <>.</> },
+  { greeting: <>Hallo, ich bin</>, punctuation: <>.</> },
+  { greeting: <>안녕하세요, 저는</>, punctuation: <>.</> },
 ];
 
 export const HeroSection = () => {
@@ -30,8 +35,11 @@ export const HeroSection = () => {
 
   return (
     <section className="min-h-screen flex items-center justify-center px-6">
-      <div className="max-w-4xl mx-auto text-center inline-block cursor-pointer">
-        <h1 className="text-5xl font-bold mb-6" onClick={handleGreetingClick}>
+      <div className="max-w-4xl mx-auto text-center inline-block">
+        <h1
+          className="text-5xl font-bold mb-6 cursor-pointer"
+          onClick={handleGreetingClick}
+        >
           <span className="inline-block">
             <GlitchText
               disabled={!isGlitching}
@@ -40,10 +48,11 @@ export const HeroSection = () => {
               color2="green"
               iterationCount={5}
             >
-              {greetings[currentGreeting]}
+              {greetings[currentGreeting].greeting}
             </GlitchText>
           </span>
           <span className="text-primary"> Shamith Pasula</span>
+          <span>{greetings[currentGreeting].punctuation}</span>
         </h1>
         <div className="flex justify-center items-center mb-8">
           <Link href="#about" className="text-4xl text-gray-300">
